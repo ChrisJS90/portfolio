2147ea0fe12e2f493dad9db01125c1cafd7fb6df
ChrisJS90 <christophersach90@gmail.com>
2025-03-12 19:02:44 +0000

    added structure for main page

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+function Header() {
+  return (
+    <div id="header-body">
+      <header>
+        <h1>Welcome</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
